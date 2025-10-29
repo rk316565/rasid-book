8d364c86d7c6ad36d68636c664cac691275f10e1
rk316565 <rk316565@gmail.com>
2025-10-29 15:07:27 +0530

    Delete public _html directory

--- public _html/api_search_customers.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config/config.php'; require_once __DIR__ . '/src/models/Customer.php';
-protect_page(); require_store_context();
-header('Content-Type: application/json');
-$term = $_GET['term'] ?? '';
-$customerModel = new Customer($pdo);
-$customers = $customerModel->searchByNameOrPhone($_SESSION['current_store_id'], $term);
-echo json_encode($customers);
